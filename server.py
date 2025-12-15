ddceb590256db63852eab741e2065e285d953c26
AlonAzran <alon.azran@gmail.com>
2025-12-15 17:09:13 +0200

    Update server.py

--- server.py
+++ server.py
@@ -393,7 +393,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:3000",
         "http://localhost:3000",
-        "https://psychometric-vocabulary-study-front-iota.vercel.app"
+        "https://v0-vocabulary-learning-app-sigma.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
